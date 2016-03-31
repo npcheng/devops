b5ec4aa4fed640f450d1ff85a3374874a60928df
npcheng <blueapache@gmail.com>
2016-03-31 16:46:56 +0800

    test.py

--- ansible/shell/test.py
+++ ansible/shell/test.py
@@ -9,10 +9,9 @@ import yaml,argparse
 
 class  wyInventory(object):
     def __init__(self):
-        self._ansbile_host = "/data/ansible/ansible_host_bak"
-        self._tengine_config = "/data/ansible/roles/tengine_config/vars"
+        self._tengine_config = config.group_vars
 
-    def get_host(self):
+    def get_host(self, project):
         http = httplib2.Http()
         headers = {
             'Content-type': 'application/x-www-form-urlencoded',
@@ -25,15 +24,17 @@ class  wyInventory(object):
                 project_url = urllib.urlopen("http://asset.wxshake.com/api/project/project").read()
                 project_en = json.loads(project_url)
         except Exception, e:
-            print( e)
+            print( e )
             sys.exit(1)
+
+        #加载project配置文件
         try:
-            f = open(self._ansbile_host, "w")
+            f_read = open(self._tengine_config, "r")
+            var_content = yaml.safe_load(f_read.read())
+            f_read.close()
         except Exception, e:
-            print(e)
-        finally:
-            print("success")
-        print( project_en)
+            print "can't open the file"
+            var_content=[]
 
         for project_list in project_en:
             project_domain = project_en[project_list]["project_domain"]
@@ -47,8 +48,22 @@ class  wyInventory(object):
             else:
                 project_en[project_list]["template_file"]= [{"src": "standard.conf.j2" ,
                              "dest":  domain + ".conf"}]
-        print yaml.safe_dump(project_en, default_flow_style=False)
-        yaml.safe_dump(project_en, f, default_flow_style=False )
+
+            project_en[project_list]["log_file"] = domain
+
+#        output_content =  yaml.safe_dump(project_en, default_flow_style=False)
+
+#        if var_content == project_en:
+#            return
+
+
+        try:
+            f = open(self._tengine_config, "w")
+        except Exception, e:
+            print(e)
+        finally:
+            print("success")
+        yaml.safe_dump(project_en[project], f, default_flow_style=False )
         f.close()
 
     def gen_playbook(self, project, action):
@@ -56,21 +71,22 @@ class  wyInventory(object):
             print "action not support"
             exit(1)
         if action == "add":
-            playbook_role = "tengine"
+            playbook_role = "tengine_config"
         else:
-            playbook_role = "tengine_del"
-            
+            playbook_role = "tengine_config_del"
+
         playbook = [{"hosts": project,"gather_facts": "no",
-                     "role":[playbook_role]
+                     "roles":[playbook_role]
                      }]
         try:
-            f = open(config.playbook_path, "w")
+            f = open(config.playbook_path, "w+")
         except Exception,e:
             print e
-	print yaml.safe_dump(playbook, default_flow_style=False)	
-	f.close()
 
+        print yaml.safe_dump(playbook, default_flow_style=False)
+        yaml.safe_dump(playbook, f,default_flow_style=False)
 
+        f.close()
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -80,9 +96,13 @@ if __name__=="__main__":
     arg = parser.parse_args()
     inventory = wyInventory()
     if arg.add !=None:
-        inventory.gen_playbook(arg.add, "add")
-    elif arg.delete != None and arg.add == None :
-        inventory.gen_playbook(arg.add, "del")
 
+        inventory.get_host(arg.add)
+        inventory.gen_playbook(arg.add, "add")
+
+    elif arg.delete != None and arg.add == None :
+        inventory.get_host(arg.delete)
+        inventory.gen_playbook(arg.delete, "del")
     else:
-        pass
+        print "亲再试试吧!"
+        exit()
