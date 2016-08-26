6bb2dae4bc37de9dfc97f06f6cfe74abab98a7dd
npcheng <blueapache@gmail.com>
2016-08-26 15:58:24 +0800

    nagios config

---
+++ nagios_plugin/config/config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+db_config={
+          "DB":{"host":"192.168.10.111",
+                   "user_name":"monitor",
+                   "passwd":"monitor",
+         }
--- nagios_plugin/config/update_nagios_memcached.py
+++ nagios_plugin/config/update_nagios_memcached.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
-import ansible.runner
-
+import ansible.runner, re, shutil, commands,os
+import config
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
 
 def run_ansible(cmd, group):
     if isinstance(group,int):
@@ -14,14 +19,33 @@ def run_ansible(cmd, group):
     ret = run.run()
     return ret['contacted']
 
-def setup_service(memcache):
+def setup_memservice(memhost):
+    global result
+    memcache = {}
+    if type(memhost) == list:
+        for host in memhost:
+            results = run_ansible("ps axf| grep memcached | grep -v grep", host)
+            for key, value in results.items():
+                print value['rc']
+                cmd_list = value['stdout'].split("\r\n")
+                for ps_list in cmd_list:
+                    if len(ps_list) != 0 :
+                        ps = ps_list.split()
+                        if key  in memcache.keys():
+                            memcache[key].append(ps[11])
+                        else:
+                            memcache[key] = []
+    else:
+        result = False
     try:
        fp = open("services.cfg", "a")
     except Exception, e:
        print e
     for key, value in memcache.items():
-        if key == "mem":
-             host = '10.249.169.202'
+        host = get_ipaddr(key)
+        if host == False:
+            result = False
+            break 
         for v in value:
             fp.write("define service {\n")
             fp.write("    use local-service\n")
@@ -31,45 +55,112 @@ def setup_service(memcache):
             fp.write("    notifications_enabled    1\n")
             fp.write("}\n")
     fp.close()
+    if result != False:
+        result = True
 
+def setup_dbservice(dbhost):
+    global result 
+    with open("services.cfg", "w") as fp:
+       for ipaddr, hostname in dbhost.items():
+           fp.write("define service {\n")
+           fp.write("    use local-service\n")
+           fp.write("    host_name %s\n" %ipaddr.replace(".", "-"))
+           fp.write("    service_description check_mysqlstatus\n")
+           fp.write("    check_command  check_mysql\n")
+           fp.write("    notifications_enabled    1\n")
+           fp.write("}\n")
+    fp.close()
 
+def dict_key_value(dbdict):
+    host= {}
+    for key, value in dbdict.items():
+         host[value['host']] = key
+    return host       
+
+def get_ipaddr(host):
+    with open("/etc/hosts", "r") as fp:
+        for line in fp.readlines():
+            match = re.search(r"(\d+\.\d+\.\d+\.\d+)\s+" + host, line)
+            if match:
+                return match.group(1)
+        return False
+            
 def setup_hostgroup( hostgroup):
-    if type(hostgroup) == "list":
+    global result
+    if type(hostgroup) == list:
         with open("hostgroups.cfg", "w") as fp:
             fp.write("define hostgroup{\r\n")
             fp.write("    hostgroup_name	jetty_instance\r\n")
             fp.write("    alias		jetty_instance\r\n")
             host_list = ",".join(hostgroup)
-            fp.write("    members    host_list")
+            fp.write("    members    %s\n" %host_list.replace(".", "-"))
             fp.write("}\r\n")
+        result = True
 
         with open("hosts.cfg", "w") as fp:
             for host in hostgroup:
                 fp.write("define host {\r\n")
                 fp.write("   use linux-server\r\n")
                 fp.write("   host_name %s\r\n" %host.replace(".", "-"))
-                fp.write("   alias DB-C-1\r\n")
-                fp.write("   address \r\n" %host)
+                fp.write("   alias %s\r\n" %db_dict[host])
+                fp.write("   address %s\r\n" %host)
                 fp.write("}\r\n")
-         
+        result = True
+    else:
+        result = False
 
 
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr(( \
+        Header(name, 'utf-8').encode(), \
+        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+
+def sendmail(result):
+    sender ="nagios@localhost"
+    recevier = ["xxx@xxx.com"]
+    message = MIMEText(result, 'plain', 'utf-8')
+    message['From'] = _format_addr("%s" %sender)
+    message['To'] =  _format_addr("%s" %recevier[0])
+    message['Subject'] = Header("nagios problem", "utf-8").encode()
+    try:
+        server = smtplib.SMTP("localhost", 25)
+        server.sendmail(sender, recevier, message.as_string())
+        print "sendmail success"
+    except smtplib.SMTPException:
+        print "sendmail failed"
 if __name__ == "__main__":
-   host = "mem"
-   memcached_info = {}
-   results = run_ansible("ps axf| grep memcached | grep -v grep", host)
-   for key, value in results.items():
-       cmd_list = value['stdout'].split("\r\n")
-       for ps_list in cmd_list:
-           if len(ps_list) != 0 :
-               ps = ps_list.split()
-               if key  in memcached_info.keys():
-                   memcached_info[key].append(ps[11])
-               else:
-                   memcached_info[key] = []
-   with open("/data/django/pjcheck/memstat/memport.py", "w") as fp:
-       mem_info = "mem_info = %s" %memcached_info
-       fp.write(mem_info +"\r\n")
-   fp.close()
-   
-   #setup_service(memcached_info)
+    ret = False
+    host = ["mem"]
+    hostgroup = []
+    src_dir = "/root/xxx"
+    dst_dir = "/etc/nagios/objects"
+    for ipaddr in host:
+        ip= get_ipaddr(ipaddr)
+        if ipaddr != False:
+            hostgroup.append(ip)
+    for key, value in config.db_config.items():
+        if value['host'] not in hostgroup:
+            hostgroup.append(value['host'])
+    db_dict = dict_key_value(config.db_config)
+    setup_hostgroup(hostgroup)
+    setup_dbservice(db_dict)
+    setup_memservice(host)
+
+    #copy file 
+    nagios_file = ['services.cfg', 'hosts.cfg', 'hostgroups.cfg']
+
+    for cfg in nagios_file:
+        try:
+            shutil.move( os.path.join(src_dir, cfg), os.path.join(dst_dir, cfg) )
+        except Exception, e:
+            print e
+
+    #check nagios file
+    (status, output) = commands.getstatusoutput("nagios -v /etc/nagios/nagios.cfg")
+    if status == 0 :
+        commands.getstatusoutput("/etc/init.d/nagios restart")
+    else:
+        sendmail(output)
